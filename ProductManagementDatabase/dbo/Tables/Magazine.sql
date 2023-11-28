5691c708f29f6a609aab65f6458e6b1cf2bd83e0
iulianp1002 <iulianp_1002@yahoo.com>
2023-11-28 14:27:37 +0200

    add first stage

---
+++ ProductManagementDatabase/dbo/Tables/Magazine.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Magazine] (
+    [CodMagazin] NVARCHAR (20)  NOT NULL,
+    [Denumire]   NVARCHAR (150) NULL,
+    [Detalii]    NVARCHAR (250) NULL,
+    CONSTRAINT [PK_Magazine] PRIMARY KEY CLUSTERED ([CodMagazin] ASC)
+);
+
